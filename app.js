d0cc87d07c29f5db31d393851b675ed58ca7897d
Manisharan-ritwik <61692773+Manisharan-ritwik@users.noreply.github.com>
2020-04-08 18:51:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+var app = angular.module("ocrApp", []);
+
+app.controller("ocrController", function($scope, $http){
+	
+	var apiKey = "AIzaSyDBMPY7gGcfyZtbSfT7g31u3vM63aqZfoo";
+	
+	var c = document.getElementById("uploadedPic");
+	c.height = 0;
+	c.width = 0;
+  	var ctx = c.getContext("2d");
+  	var img = new Image();
+
+  	$scope.getResult = function(){
+  		$scope.vision_api_json = {
+		      "requests":[
+			{
+			  "image":{
+				"content" : $scope.imgBase64
+			  },
+			  "features":[
+			    {
+			      "type": "TEXT_DETECTION",
+			      "maxResults": 1
+			    }
+			  ]
+			}
+		      ]
+		};
+
+  		$http({
+			url : "https://vision.googleapis.com/v1/images:annotate?key="+apiKey,
+			method : "POST",
+			data : $scope.vision_api_json
+		}).then(function(response){
+
+			var detectedText = response.data.responses[0].fullTextAnnotation.text;
+
+			detectedText = detectedText.split("FLAVOR")[0] + "FLAVOR";
+
+			$scope.result = detectedText;
+
+		});
+  	}
+
+	$scope.uploadPicture = function (ele) {
+	  	img.src = URL.createObjectURL(ele.files[0]); 
+	    	img.onload = function() {
+			c.height = img.height/2;
+			c.width = img.width/2;
+			//draw selected image to canvas
+			ctx.drawImage(img, 0, 0, c.width, c.height);
+			//get base64 and set to result section
+			var result = c.toDataURL();
+			$scope.imgBase64 = result.replace('data:image/png;base64,','', result);
+			//alert($scope.imgBase64);
+			$scope.$apply();
+			$scope.getResult();
+		}
+	}
+})
