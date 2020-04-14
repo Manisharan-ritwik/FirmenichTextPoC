2947f9130fd92ec16a65c30c05073f613a701636
Manisharan-ritwik <61692773+Manisharan-ritwik@users.noreply.github.com>
2020-04-14 12:15:56 +0530

    Changed api Key

--- FirmenichTextPoC/app.js
+++ FirmenichTextPoC/app.js
@@ -2,7 +2,7 @@ var app = angular.module("ocrApp", []);
 
 app.controller("ocrController", function($scope, $http){
 	
-	var apiKey = "AIzaSyDBMPY7gGcfyZtbSfT7g31u3vM63aqZfoo";
+	var apiKey = "AIzaSyAWd1Ts-xqQMrvZDT62VXkXw0PgJlfSF-c";//"AIzaSyDBMPY7gGcfyZtbSfT7g31u3vM63aqZfoo";
 	
 	var c = document.getElementById("uploadedPic");
 	c.height = 0;
