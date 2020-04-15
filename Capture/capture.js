9cbdd8756175bc5618972435a7d60a3e259caa7a
ManishQubetree <manisharan18@gmail.com>
2020-04-15 18:53:31 +0530

    Added pick and strintourl filter

---
+++ Capture/capture.js
@@ -0,0 +1,155 @@
+(function() {
+  // The width and height of the captured photo. We will set the
+  // width to the value defined here, but the height will be
+  // calculated based on the aspect ratio of the input stream.
+
+  var width = 320;    // We will scale the photo width to this
+  var height = 0;     // This will be computed based on the input stream
+
+  // |streaming| indicates whether or not we're currently streaming
+  // video from the camera. Obviously, we start at false.
+
+  var streaming = false;
+
+  // The various HTML elements we need to configure or control. These
+  // will be set by the startup() function.
+
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+
+  function startup() {
+
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('startbutton');
+
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function(err) {
+      console.log("An error occurred: " + err);
+    });
+
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+      
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
+      
+        if (isNaN(height)) {
+          height = width / (4/3);
+        }
+      
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+      ev.preventDefault();
+    }, false);
+    
+    clearphoto();
+  }
+
+
+  // Fill the photo with an indication that none has been
+  // captured.
+
+  function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+  
+  // Capture a photo by fetching the current contents of the video
+  // and drawing it into a canvas, then converting that to a PNG
+  // format data URL. By drawing it on an offscreen canvas and then
+  // drawing that to the screen, we can change its size and/or apply
+  // other changes before drawing it.
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+
+      data = data.replace('data:image/png;base64,','');
+
+
+            // Post a user
+      var url = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDBMPY7gGcfyZtbSfT7g31u3vM63aqZfoo";
+
+      var data = {
+        "requests": [
+          {
+            "image": {
+              "content": data
+            },
+            "features": [
+              {
+                "type": "TEXT_DETECTION",
+                "maxResults": 1
+              }
+            ]
+          }
+        ]
+      };
+      var json = JSON.stringify(data);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+      xhr.onload = function () {
+
+        var response = JSON.parse(xhr.responseText);
+
+        var result = response.responses[0].fullTextAnnotation.text;// "882134 TFS1287 TEA FLEXAROME FLAVOR LOTNO: 1003620774 -86010478 FP> 100 C UNO: EXP DATE 180 Lot: HO17"
+
+      result = result.split("FLAVOR")[0]+"FLAVOR";
+
+      result = result.replace(/\s/g,'');
+
+      result = result.toLowerCase();
+
+      result = "http://ritwiksoftware.com/clients/firmenich_dev/?producname="+result;
+
+      document.getElementById("resultdisplay").innerHTML = result;
+      document.getElementById("resulturl").href = result;
+
+      }
+      xhr.send(json); 
+
+
+
+
+    } else {
+      clearphoto();
+    }
+
+
+
+
+  }
+
+  // Set up our event listener to run the startup process
+  // once loading is complete.
+  window.addEventListener('load', startup, false);
+})();
--- Pick/app.js
+++ Pick/app.js
@@ -2,13 +2,13 @@ var app = angular.module("ocrApp", []);
 
 app.controller("ocrController", function($scope, $http){
 	
-	var apiKey = "AIzaSyAWd1Ts-xqQMrvZDT62VXkXw0PgJlfSF-c";//"AIzaSyDBMPY7gGcfyZtbSfT7g31u3vM63aqZfoo";
+	var apiKey = "AIzaSyDBMPY7gGcfyZtbSfT7g31u3vM63aqZfoo";
 	
 	var c = document.getElementById("uploadedPic");
-	c.height = 0;
-	c.width = 0;
   	var ctx = c.getContext("2d");
   	var img = new Image();
+
+  	$scope.result = "Result will be displayed here";
 
   	$scope.getResult = function(){
   		$scope.vision_api_json = {
@@ -33,11 +33,20 @@ app.controller("ocrController", function($scope, $http){
 			data : $scope.vision_api_json
 		}).then(function(response){
 
-			var detectedText = response.data.responses[0].fullTextAnnotation.text;
+			var r = response.data.responses[0].fullTextAnnotation.text;
+			r = r.split("FLAVOR")[0]+"FLAVOR";
 
-			detectedText = detectedText.split("FLAVOR")[0] + "FLAVOR";
+		      r = r.replace(/\s/g,'');
 
-			$scope.result = detectedText;
+		      r = r.toLowerCase();
+
+		      r = "http://ritwiksoftware.com/clients/firmenich_dev/?producname="+r;
+
+		      document.getElementById("result").innerHTML = r;
+		      document.getElementById("resulturl").href = r;
+			//detectedText = detectedText.split("FLAVOR")[0] + "FLAVOR";
+
+			//$scope.result = detectedText;
 
 		});
   	}
@@ -45,10 +54,8 @@ app.controller("ocrController", function($scope, $http){
 	$scope.uploadPicture = function (ele) {
 	  	img.src = URL.createObjectURL(ele.files[0]); 
 	    	img.onload = function() {
-			c.height = img.height/2;
-			c.width = img.width/2;
-			//draw selected image to canvas
-			ctx.drawImage(img, 0, 0, c.width, c.height);
+			ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
+                   0, 0, c.width, c.height);
 			//get base64 and set to result section
 			var result = c.toDataURL();
 			$scope.imgBase64 = result.replace('data:image/png;base64,','', result);
